9be022a46b087aa594e6b4b09c5801cf6dfcda5d
심세영 <ssiox3@gmail.com>
2020-04-01 01:26:05 +0900

    Mobx 의 Context에 Provider추가.

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { useStore } from '../stores/indexStore';
+import { useLocalStore } from 'mobx-react';
 
 const storeContext = React.createContext(useStore);
+
+export const StoreProvider = children => {
+  const store = useLocalStore(() => ({
+    bugs: ['Centipede'],
+    addBug: bug => {
+      store.bugs.push(bug);
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    },
+  }));
+
+  return (
+    <storeContext.Provider value={store}>{children}</storeContext.Provider>
+  );
+};
 
 export const UseStore = () => {
   const store = React.useContext(storeContext);
